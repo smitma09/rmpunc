e1e66fbbad867b45327222fb0f2ce271c014a954
Matthew Alan Smith <smitma09@8258.local>
2015-10-20 13:37:50 -0500

    Added a comment...

--- rmpunc.c
+++ rmpunc.c
@@ -4,7 +4,7 @@
 int main(int argc, char* argv[]) {
 
   char c;
-
+/* Either way below is equivalent. They both do the same thing...
 /* These 2 different while loops do the same thing
   c = getchar();
   while (c != EOF) {
